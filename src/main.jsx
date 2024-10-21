cf32510ae4d50e6bbb3c4b36d7e2b675c363131e
Hanumamt Dombale <hanumantdombale60@gmail.com>
2024-10-21 16:12:34 +0530

    update file

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,36 @@
 import {
-    BrowserRouter,
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import "./index.css";
 import {
-    Home,
-    About,
-    Contact,
-    fetchGithubData,
-    Github,
-    User,
-    Layout,
+  Home,
+  About,
+  Contact,
+  fetchGithubData,
+  Github,
+  User,
+  Layout
 } from "./components";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Layout />}>
-            <Route path="" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route
-                path="github"
-                element={<Github />}
-                loader={fetchGithubData}
-            />
-            <Route path="user/:id" element={<User />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="github" element={<Github />} loader={fetchGithubData} />
+      <Route path="user/:id" element={<User />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <BrowserRouter basename="/simple-website/">
-            <RouterProvider router={router} />
-        </BrowserRouter>
-    </StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
